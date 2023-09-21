684b34cc38f44f84352ecfd0432a3429274953a3
MurilloDiana <dianamurillo.467@gmail.com>
2023-09-20 22:22:44 -0400

    metronomo
    funcion de metronomo

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MetronomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('metronome')->group(function () {
+    Route::post('start', [MetronomeController::class, 'start']);
+    Route::post('stop', [MetronomeController::class, 'stop']);
+    Route::post('tempo', [MetronomeController::class, 'setTempo']);
 });
